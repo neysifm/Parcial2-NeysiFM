cbc0c082c8a6a1afe5fac137b1047f8ef1d9e85f
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-26 16:21:40 -0400

    Registro de Estudiantes

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Parcial2_NeysiFM.Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Parcial2_NeysiFM.DAL
 {
-    public class Contexto
+    public class Contexto: DbContext
     {
+        public DbSet<Asignaturas> Asignatura { get; set; }
+        public DbSet<Estudiantes> Estudiante { get; set; }
+        public DbSet<Incripciones> Incripcion { get; set; }
+        public Contexto() : base("Constr")
+        {
+        }     
     }
 }
---
+++ UI/Registros/RegistroEstudiantes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial2_NeysiFM.UI.Registros
+{
+    public partial class RegistroEstudiantes : MetroFramework.Forms.MetroForm
+    {
+        public RegistroEstudiantes()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistroEstudiantes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
