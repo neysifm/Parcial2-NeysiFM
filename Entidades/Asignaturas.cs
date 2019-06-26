bfd71ca5cbb6026d6725b4f6491178fa8a870d7c
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-26 15:54:33 -0400

    Entidad Asignaturas

---
+++ Entidades/Asignaturas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_NeysiFM.Entidades
+{
+    public class Asignaturas
+    {
+        public int AsignaturaId { get; set; }
+        public string Descripcion { get; set; }
+        public int Creditos { get; set; }
+
+        public Asignaturas(int asignaturaId, string descripcion, int creditos)
+        {
+            AsignaturaId = asignaturaId;
+            Descripcion = descripcion;
+            Creditos = creditos;
+        }
+
+        public Asignaturas()
+        {
+            AsignaturaId = 0;
+            Descripcion = string.Empty;
+            Creditos = 0;
+        }
+    }
+    
+}
