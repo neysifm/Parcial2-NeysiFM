19e4ee3449a14619729736bd869aae03de557337
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-26 16:27:06 -0400

    Completando Estructura Requerida en Programacion Aplicada 1

---
+++ UI/Reportes/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_NeysiFM.UI.Reportes
+{
+    class Class1
+    {
+        // COMPLETANDO ESTRUCTURA PROYECTO SEGUNDO PARCIAL PROGRAMACION APLICADA 1
+    }
+}
