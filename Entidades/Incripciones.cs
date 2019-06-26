ac6ca9559e4b88f53b67042b7f07a6a5b490ebe8
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-06-26 16:08:07 -0400

    Entidad Incripciones

---
+++ Entidades/Incripciones.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_NeysiFM.Entidades
+{
+    public class Incripciones
+    {
+        public int IncripcionId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+
+        public Incripciones(int incripcionId, DateTime fecha, double monto)
+        {
+            IncripcionId = incripcionId;
+            Fecha = fecha;
+            Monto = monto;
+        }
+
+        public Incripciones()
+        {
+            IncripcionId = 0;
+            Fecha = DateTime.Now;
+            Monto = 0;
+        }
+    }
+}
